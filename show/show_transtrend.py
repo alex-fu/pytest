ccf6a97178b630bc53f47aeda9cf5997b1c8b079
Fu Yifeng <yfeng.fu@vpon.com>
2016-11-24 18:01:25 +0800

    refine show_transtrend

--- show/show_transtrend.py
+++ show/show_transtrend.py
@@ -2,12 +2,17 @@
 from couchbase.bucket import Bucket
 from couchbase.n1ql import N1QLQuery
 from flask import Flask
+from flask_cors import CORS
 from flask_restful import Api, Resource
+from flask_restful import reqparse
+from functools import wraps
+from flask import make_response
 
 from strategy.config import ST_DBURL, STRATEGY_BUCKET_NAME
 from fn import _
 
 app = Flask(__name__)
+CORS(app, supports_credentials=True)
 api = Api(app)
 cb = Bucket(ST_DBURL)
 
@@ -16,18 +21,42 @@ STVER = 'v1'
 
 
 class TranstrendAPI(Resource):
-    def get(self):
-        trades = get_trades_2016_all()
-        r = map(lambda x: {'date': x, 'rate': trades[x]}, trades)
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('year', type=str, location='args')
+        self.reqparse.add_argument('ndayclose', type=int, location='args')
 
+    def get(self):
+        args = self.reqparse.parse_args()
+        year = args['year']
+        ndayclose = args['ndayclose']
+
+        trades = None
+        if year and int(year) in range(2011, 2017):
+            trades = get_trades_by_year(year, ndayclose)
+        else:
+            trades = get_trades_all(ndayclose)
+
+        r = map(lambda x: {'date': x, 'rate': trades[x]}, sorted(trades.keys()))
         return {'result': list(r)}
 
 
-def get_trades_2016_all():
+def get_trades_all(ndayclose):
+    r2011 = get_trades_by_year('2011', ndayclose)
+    r2012 = get_trades_by_year('2012', ndayclose)
+    r2013 = get_trades_by_year('2013', ndayclose)
+    r2014 = get_trades_by_year('2014', ndayclose)
+    r2015 = get_trades_by_year('2015', ndayclose)
+    r2016 = get_trades_by_year('2016', ndayclose)
+    return {**r2011, **r2012, **r2013, **r2014, **r2015, **r2016}
+
+
+def get_trades_by_year(year, ndayclose):
     cql = 'SELECT trades FROM `' + STRATEGY_BUCKET_NAME + \
           '` WHERE strategy_name=$stname and strategy_version=$stver' + \
-          ' and startdate=$startdate and enddate=$enddate'
-    r = cb.n1ql_query(N1QLQuery(cql, stname=STNAME, stver=STVER, startdate='2016-01-01', enddate='2016-12-31'))
+          ' and startdate=$startdate and enddate=$enddate and ndayclose=$ndayclose'
+    r = cb.n1ql_query(N1QLQuery(cql, stname=STNAME, stver=STVER, startdate=year + '-01-01', enddate=year + '-12-31',
+                                ndayclose=ndayclose))
     r1 = {}
     for x in r:
         trades = x['trades']
---
+++ test_transtrend.sh
@@ -0,0 +1,6 @@
+python -m strategy.st_stock_1 -s '2011-01-01' -e '2011-12-31' -n 5 -p 0.05
+python -m strategy.st_stock_1 -s '2012-01-01' -e '2012-12-31' -n 5 -p 0.05
+python -m strategy.st_stock_1 -s '2013-01-01' -e '2013-12-31' -n 5 -p 0.05
+python -m strategy.st_stock_1 -s '2014-01-01' -e '2014-12-31' -n 5 -p 0.05
+python -m strategy.st_stock_1 -s '2015-01-01' -e '2015-12-31' -n 5 -p 0.05
+python -m strategy.st_stock_1 -s '2016-01-01' -e '2016-12-31' -n 5 -p 0.05
